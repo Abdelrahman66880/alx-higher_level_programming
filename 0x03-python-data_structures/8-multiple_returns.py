6200f8d125d0253d5e701e1ff872e6509a62ea11
Abdelrahman <abdofathi317@gmail.com>
2023-12-04 14:32:24 -0800

    Lists and Tuples

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    my_typle = ()
+    if sentence == "":
+        length = 0
+        frist  = None
+        my_typle = (length, frist)
+        return (my_typle)
+    else:
+        length = len(sentence)
+        frist = sentence[0]
+        my_typle = (length, frist)
+    return (my_typle)
