45424ead30361d270f9e7afd7597f770d6469ed8
Abdelrahman <abdofathi317@gmail.com>
2023-12-07 01:57:00 -0800

    Sets and Dictionary

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
-def common_elements(set_1, set_2):
-    intersection_1 = set_1.intersection(set_2)
-    union_1 = set_1.union(set_2)
-    difference = union_1.difference(intersection_1)
-    return (difference)
+def only_diff_elements(set_1, set_2):
+    """
+    A function that returns a set of all elements
+    present in only one set.
+    """
+    return (set_1 ^ set_2)
