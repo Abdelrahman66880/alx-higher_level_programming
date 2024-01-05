c3c4f039e77bf50a05847db4d1630fdebc452fcf
Abdelrahman <abdofathi317@gmail.com>
2024-01-05 14:12:00 -0800

    More Classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Define an empty class called rectangle
+"""
+
+class Rectangle:
+    """Empty representation of Rectangle"""
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is my README file about more classes task
